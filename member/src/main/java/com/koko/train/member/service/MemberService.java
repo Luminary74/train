985795cde46a1bbb6963d1c71eab41237f7fac8d
Aur4r0 <aur4r0@gmail.com>
2025-03-12 20:55:03 +0800

    使用雪花算法生成数据库ID

--- member/src/main/java/com/koko/train/member/service/MemberService.java
+++ member/src/main/java/com/koko/train/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.koko.train.member.service;
 import cn.hutool.core.collection.CollUtil;
 import com.koko.train.common.exception.BusinessException;
 import com.koko.train.common.exception.BusinessExceptionEnum;
+import com.koko.train.common.util.SnowUtil;
 import com.koko.train.member.domain.Member;
 import com.koko.train.member.domain.MemberExample;
 import com.koko.train.member.mapper.MemberMapper;
@@ -35,7 +36,7 @@ public class MemberService {
 
 
         Member member = new Member();
-        member.setId(System.currentTimeMillis());
+        member.setId(SnowUtil.getSnowflakeNextId());
         member.setMobile(mobile);
         memberMapper.insert(member);
         return member.getId();
