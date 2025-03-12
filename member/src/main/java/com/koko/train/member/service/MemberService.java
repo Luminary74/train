d0f1c72373f656a273a222c50581ae3b9370e0dc
Aur4r0 <aur4r0@gmail.com>
2025-03-12 18:02:59 +0800

    实现一个简单的注册接口

--- member/src/main/java/com/koko/train/member/service/MemberService.java
+++ member/src/main/java/com/koko/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.koko.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.koko.train.member.domain.Member;
+import com.koko.train.member.domain.MemberExample;
 import com.koko.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -12,5 +17,23 @@ public class MemberService {
 
     public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    //注册服务
+    public long register(String mobile) {
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+
+        if (CollUtil.isNotEmpty(list)) {
+            throw new RuntimeException("手机已注册");
+        }
+
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
