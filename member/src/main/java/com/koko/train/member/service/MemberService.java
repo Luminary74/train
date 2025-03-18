e926def035db57a471e2a3896248b076d086191d
Aur4r0 <aur4r0@gmail.com>
2025-03-18 17:40:07 +0800

    使用hutool生成JWT token

--- member/src/main/java/com/koko/train/member/service/MemberService.java
+++ member/src/main/java/com/koko/train/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.koko.train.member.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.RandomUtil;
+import cn.hutool.jwt.JWTUtil;
 import com.koko.train.common.exception.BusinessException;
 import com.koko.train.common.exception.BusinessExceptionEnum;
 import com.koko.train.common.util.SnowUtil;
@@ -19,6 +20,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 
 @Service
@@ -76,7 +78,7 @@ public class MemberService {
         LOG.info("对接短信通道");
     }
 
-    //验证码服务
+    //登录服务
     public MemberLoginResp login(MemberLoginReq req) {
         String mobile = req.getMobile();
         String code = req.getCode();
@@ -92,7 +94,13 @@ public class MemberService {
             throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_CODE_ERROR);
         }
 
-        return BeanUtil.copyProperties(memberDB, MemberLoginResp.class);
+
+        MemberLoginResp memberLoginResp = BeanUtil.copyProperties(memberDB, MemberLoginResp.class);
+        Map<String,Object> map =  BeanUtil.beanToMap(memberLoginResp);
+        String key = "koko12306";
+        String token = JWTUtil.createToken(map, key.getBytes());
+        memberLoginResp.setToken(token);
+        return memberLoginResp;
     }
 
     private Member selectByMobile(String mobile) {
