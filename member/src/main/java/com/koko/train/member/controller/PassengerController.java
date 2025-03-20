4d53a90bd299c737df93a2989c97a49d615e1f61
Aur4r0 <aur4r0@gmail.com>
2025-03-20 10:42:40 +0800

    增加乘车人新增接口

---
+++ member/src/main/java/com/koko/train/member/controller/PassengerController.java
@@ -0,0 +1,27 @@
+package com.koko.train.member.controller;
+
+
+import com.koko.train.common.resp.CommonResp;
+import com.koko.train.member.req.PassengerSaveReq;
+import com.koko.train.member.service.PassengerService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/passenger")
+public class PassengerController {
+    @Resource
+    private PassengerService passengerService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq req) {
+        passengerService.save(req);
+        return new CommonResp<>();
+    }
+
+
+}
