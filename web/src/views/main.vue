80787f363034aa6c6f2d79c6d67adae5011b82d2
Aur4r0 <aur4r0@gmail.com>
2025-03-20 11:56:54 +0800

    制作欢迎界面

--- web/src/views/main.vue
+++ web/src/views/main.vue
@@ -3,28 +3,18 @@
     <the-header-view></the-header-view>
     <a-layout>
       <the-sider-view></the-sider-view>
-      <a-layout style="padding: 0 24px 24px">
-        <a-breadcrumb style="margin: 16px 0">
-          <a-breadcrumb-item>Home</a-breadcrumb-item>
-          <a-breadcrumb-item>List</a-breadcrumb-item>
-          <a-breadcrumb-item>App</a-breadcrumb-item>
-        </a-breadcrumb>
-        <a-layout-content
-            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-        >
-          所有会员总数： {{count}}
-        </a-layout-content>
-      </a-layout>
+      <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+      >
+        <router-view></router-view>
+      </a-layout-content>
     </a-layout>
   </a-layout>
 </template>
-
 <script>
-import {defineComponent, ref} from 'vue';
-import TheHeaderView from "@/components/the-header.vue";
-import TheSiderView from "@/components/the-sider.vue";
-import axios from "axios";
-import {notification} from "ant-design-vue";
+import { defineComponent } from 'vue';
+import TheHeaderView from "@/components/the-header";
+import TheSiderView from "@/components/the-sider";
 
 export default defineComponent({
   components: {
@@ -32,40 +22,11 @@ export default defineComponent({
     TheHeaderView,
   },
   setup() {
-    const count = ref(0);
-    axios.get("/member/member/count").then(response => {
-      let data = response.data
-      if (data.success) {
-        count.value = data.content;
-      } else {
-        notification.error({description: data.message});
-      }
-    });
-
-      return {
-        count,
-      };
-    },
+    return {
+    };
+  },
 });
-
 </script>
+<style>
 
-
-<style scoped>
-#components-layout-demo-top-side-2 .logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
-
-.ant-row-rtl #components-layout-demo-top-side-2 .logo {
-  float: right;
-  margin: 16px 0 16px 24px;
-}
-
-.site-layout-background {
-  background: #fff;
-}
 </style>
---
+++ web/src/views/main/welcome.vue
@@ -0,0 +1,21 @@
+
+
+<template>
+  <h1>欢迎使用风起12306售票系统</h1>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  setup(){
+    return{
+
+    };
+  },
+});
+</script>
+
+<style>
+
+</style>
