3c57e9f481a88456b322d7005358869615b0865c
Aur4r0 <aur4r0@gmail.com>
2025-03-20 18:17:32 +0800

    乘车人新增界面开发，完成

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -22,14 +22,10 @@
 
 <script>
 import {defineComponent, reactive, ref} from "vue";
-import passenger from "@/views/main/passenger.vue";
+import {notification} from "ant-design-vue";
+import axios from "axios";
 
 export default defineComponent({
-  computed: {
-    passenger() {
-      return passenger
-    }
-  },
   setup(){
     const visible = ref(false);
     const passenger = reactive({
@@ -45,10 +41,17 @@ export default defineComponent({
       visible.value = true;
     };
 
-    const handleOk = e => {
-      console.log(e);
-      visible.value = false;
-    }
+    const handleOk = () => {
+      axios.post("/member/passenger/save", passenger).then((response) => {
+        let data = response.data;
+        if (data.success) {
+          notification.success({description: "保存成功！"});
+          visible.value = false;
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
     return{
       passenger,
       visible,
