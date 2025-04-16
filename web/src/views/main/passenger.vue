7773292c8da22ae7f060bb592476e1b9262e9edb
Aur4r0 <aur4r0@gmail.com>
2025-04-16 19:57:48 +0800

    乘车人编辑界面开发，解决没保存也会同步修改列表的问题

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -81,11 +81,12 @@ export default defineComponent({
     }];
 
     const onAdd = () =>{
+      passenger.value = {};
       visible.value = true;
     };
 
     const onEdit = (record) =>{
-      passenger.value = record;
+      passenger.value = window.Tool.copy(record);
       visible.value = true;
     };
 
