45c81d576eb143510afcdbc6e2916da11cf03de8
Aur4r0 <aur4r0@gmail.com>
2025-04-16 18:23:45 +0800

    乘车人列表查询界面开发，增加刷新按钮，点击查询第1页

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -1,6 +1,9 @@
 <template>
   <p>
-    <a-button type="primary" @click="showModal()">新增</a-button>
+    <a-space>
+      <a-button type="primary" @click="handleQuery()">刷新</a-button>
+      <a-button type="primary" @click="showModal()">新增</a-button>
+    </a-space>
   </p>
   <a-table :dataSource="passengers" :columns="columns" :pagination="pagination" @change="handleTableChange"/>
   <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk"
@@ -75,6 +78,12 @@ export default defineComponent({
     };
 
     const handleQuery = (param) => {
+      if(!param){
+        param = {
+          page: 1,
+          size: pagination.pageSize,
+        };
+      }
       axios.get("/member/passenger/query-list", {
         params: {
           page: param.page,
@@ -121,6 +130,7 @@ export default defineComponent({
       columns,
       pagination,
       handleTableChange,
+      handleQuery,
     };
   },
 });
