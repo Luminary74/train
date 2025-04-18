46416335d1fc62a866a111b9d0f34de1102fa35e
Aur4r0 <aur4r0@gmail.com>
2025-04-18 19:29:16 +0800

    使用PassengerService制作模板，生成service成功

--- generator/src/main/java/com/koko/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/koko/train/generator/server/ServerGenerator.java
@@ -1,5 +1,6 @@
 package com.koko.train.generator.server;
 
+import com.koko.train.generator.util.FreemarkerUtil;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Node;
@@ -11,19 +12,22 @@ import java.util.Map;
 
 public class ServerGenerator {
 
-    static String toPath = "generator\\src\\main\\java\\com\\koko\\train\\generator\\test\\";
-
+    static String servicePath = "[module]/src/main/java/com/koko/train/[module]/service/";
     static String pomPath = "generator\\pom.xml";
-
-
-
+    static String module = "";
     static {
-        new File(toPath).mkdirs();
+        new File(servicePath).mkdirs();
     }
 
     public static void main(String[] args) throws Exception {
         // 获取mybatis-generator
         String generatorPath = getGeneratorPath();
+        // 比如generator-config-member.xml，得到module = member
+        module = generatorPath.replace("src/main/resources/generator-config-", "").replace(".xml", "");
+        System.out.println("module: " + module);
+        servicePath = servicePath.replace("[module]", module);
+        new File(servicePath).mkdirs();
+        System.out.println("servicePath: " + servicePath);
 
         // 读取table节点
         Document document = new SAXReader().read("generator/" + generatorPath);
@@ -32,6 +36,24 @@ public class ServerGenerator {
         Node tableName = table.selectSingleNode("@tableName");
         Node domainObjectName = table.selectSingleNode("@domainObjectName");
         System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+
+        // 示例：表名 koko_test
+        // Domain = KokoTest
+        String Domain = domainObjectName.getText();
+        // domain = kokoTest
+        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
+        // do_main = koko-test
+        String do_main = tableName.getText().replaceAll("_", "-");
+
+        // 组装参数
+        Map<String, Object> param = new HashMap<>();
+        param.put("Domain", Domain);
+        param.put("domain", domain);
+        param.put("do_main", do_main);
+        System.out.println("组装参数：" + param);
+
+        FreemarkerUtil.initConfig("service.ftl");
+        FreemarkerUtil.generator(servicePath + Domain +  "Test1Service.java",param);
     }
 
     private static String getGeneratorPath() throws DocumentException {
